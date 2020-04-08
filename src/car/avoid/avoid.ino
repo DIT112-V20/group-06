76f4ffe44f5dd4d53978567fd2fec8cba9c25656
hashmati <hashmati@student.chalmers.se>
2020-04-08 11:53:22 +0200

    Create spin dance move
    Changed from SimpleCar to HeadingCar because we added the gyroscope. Created the method for the spin dance move which sets the speed to 20 and rotates the car 360 degrees.
    
    Resolves #12

--- src/car/avoid/avoid.ino
+++ src/car/avoid/avoid.ino
@@ -3,13 +3,15 @@
 BrushedMotor leftMotor(smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(smartcarlib::pins::v2::rightMotorPins);
 DifferentialControl control(leftMotor, rightMotor);
-SimpleCar car(control);
+HeadingCar car(control, gyro);
 
 const int TRIGGER_PIN = 5;
 const int ECHO_PIN = 18;
 const unsigned int MAX_DISTANCE = 100;
 SR04 sensor(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
+const int GYROSCOPE_OFFSET = 32;
+GY50 gyro(GYROSCOPE_OFFSET);
 
 void setup() {
 
@@ -20,6 +22,14 @@ void loop(){
     if (distance != 0 && distance < 20){
         car.setSpeed(0);
     } else {
-        car.setSpeed(40);
+        /*car.setSpeed(40);*/
+        spin();
     }
+}
+/*
+ * Sets the speed to 20 and rotates the car 360 degrees
+ */
+void spin(){
+    car.setSpeed(20);
+    car.rotate(360);
 }
