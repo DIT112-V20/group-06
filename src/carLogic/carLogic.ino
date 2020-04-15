a1d4f4b64d9acf7233c589351e36e01bce26f782
Jennifer Nord <5363215+JenniNord@users.noreply.github.com>
2020-04-15 14:36:55 +0200

    Refactor car arduino file
    Added missing comments, removed unnecessary comments and moved some magic numbers to variables instead. Changed the name of the arduino file and folder.

---
+++ src/carLogic/carLogic.ino
@@ -0,0 +1,99 @@
+#include <Smartcar.h>
+
+BrushedMotor leftMotor(smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+
+const int TRIGGER_PIN = 5;
+const int ECHO_PIN = 18;
+const unsigned int MAX_DISTANCE = 100;
+SR04 sensor(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+const int GYROSCOPE_OFFSET = 32;
+GY50 gyro(GYROSCOPE_OFFSET);
+
+const unsigned long leftPulsesPerMeter = 943;
+const unsigned long rightPulsesPerMeter = 981;
+DirectionalOdometer leftOdometer (smartcarlib::pins::v2::leftOdometerPins, [](){
+    leftOdometer.update();
+  },
+  leftPulsesPerMeter);
+DirectionalOdometer rightOdometer (smartcarlib::pins::v2::rightOdometerPins, [](){
+    rightOdometer.update();
+  },
+  rightPulsesPerMeter);
+
+SmartCar car(control, gyro, leftOdometer, rightOdometer);
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    unsigned int distance = sensor.getDistance();
+    
+    if (distance != 0 && distance < 20){
+        car.setSpeed(0);
+    } else {
+        /* car.setSpeed(40); */
+        shuffle(30);
+    }
+}
+
+/**
+ * Spins the car on the spot
+ */
+void spin(int speed) {
+    car.setSpeed(speed);
+    control.overrideMotorSpeed(100, -100);    
+}
+
+/**
+ * This method rotates the car to the specified degree
+ */
+void rotate(int degrees) {
+    car.setAngle(degrees);
+}
+
+/**
+ * Shuffle dance move
+ * Car moves forwards and backwards (at set intervals) at the specified speed.
+ */
+void shuffle(int speed) {
+  const int shortDistance = 10;
+  const int mediumDistance = 20;
+  const int longDistance = 30;
+  
+  const long startingPoint = leftOdometer.getDistance();
+  bool danceIsFinished = false;
+  int steps = 1;
+  
+  car.setSpeed(speed);
+  
+  while(!danceIsFinished) {
+    if ((steps == 1 || steps == 5) && (leftOdometer.getDirection() == 1) && ((car.getDistance() - startingPoint) == mediumDistance)) {
+      changeDirection(); /* backwards*/
+      steps++;
+    } else if ((steps == 2 || steps == 4) && (leftOdometer.getDirection() == -1) && ((car.getDistance() - startingPoint) == shortDistance)) {
+      changeDirection(); /* forwards */
+      steps++;
+    } else if ((steps == 3) && (leftOdometer.getDirection() == 1) && ((car.getDistance() - startingPoint) == longDistance)) {
+      changeDirection(); /* backwards*/
+      steps++;
+    } else if ((steps == 6) && ((car.getDistance() - startingPoint) == 0)) {
+      danceIsFinished = true;
+      car.setSpeed(0);
+    }
+  }
+}
+
+/**
+ * Changes the direction the car moves in
+ */
+void changeDirection(int speed) {
+  if (leftOdometer.getDirection() == 1) {
+    car.setSpeed(speed * -1);
+  } else {
+    car.setSpeed(speed);
+  }
+}
