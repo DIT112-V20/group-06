9131aca5c2c159be30f002d1cfb09627e9dabb05
Jennifer Nord <5363215+JenniNord@users.noreply.github.com>
2020-04-28 16:11:29 +0200

    Connect app to the internet
    We worked on connecting the app to the internet by using OkHttp. It works but needs some tidying up as well as being able to print from the onResponse so we can get the HTTP codes for the responses.
    
    Resolves #31

---
+++ src/app/app/src/main/java/com/example/djsmartcar/activity/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.djsmartcar.activity
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.djsmartcar.R
+import okhttp3.*
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+
+    private val client = OkHttpClient()
+    private var textView: TextView? = null
+    private var mHandler = Handler(Looper.getMainLooper())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.textview)
+
+        run("https://google.com/")
+    }
+
+    private fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        var response = client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+//                    mHandler.post(Runnable() {
+//                        override fun run() {
+//                            textView?.setText("Test")
+//                        }
+//                    })
+                }
+            }
+        })
+
+        println(response)
+
+
+
+//        var response = client.newCall(request).enqueue(object : Callback {
+//            override fun onFailure(call: Call, e: IOException) {}
+//            override fun onResponse(call: Call, response: Response) {
+//                println(response.code)
+//            }
+//        })val textview = findViewById<TextView>(R.id.textview)
+//            textview?.text = response.code.toString()
+
+    }
+}
