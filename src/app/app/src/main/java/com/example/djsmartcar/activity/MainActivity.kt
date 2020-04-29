2a7ebf73482116b31c001cbab396558ae7f920c5
hashmati <negin_h@icloud.com>
2020-04-29 14:13:38 +0200

    Clean up MainActivity code
    Removed commented out code, unused variables and imports and changed a method name.
    
    Resolves #31

--- src/app/app/src/main/java/com/example/djsmartcar/activity/MainActivity.kt
+++ src/app/app/src/main/java/com/example/djsmartcar/activity/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.djsmartcar.activity
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.djsmartcar.R
@@ -14,17 +12,16 @@ class MainActivity : AppCompatActivity() {
 
     private val client = OkHttpClient()
     private var textView: TextView? = null
-    private var mHandler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.textview)
 
-        run("https://google.com/")
+        connect("https://google.com/")
     }
 
-    private fun run(url: String) {
+    private fun connect(url: String) {
         val request = Request.Builder()
             .url(url)
             .build()
@@ -37,27 +34,10 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response) {
                 response.use {
                     if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
                     this@MainActivity.runOnUiThread(java.lang.Runnable { textview.text = response.code.toString() })
-//                    mHandler.post(Runnable() {
-//                        override fun run() {
-//                            textView?.setText("Test")
-//                        }
-//                    })
                 }
             }
         })
-
-        println(response)
-
-
-
-//        var response = client.newCall(request).enqueue(object : Callback {
-//            override fun onFailure(call: Call, e: IOException) {}
-//            override fun onResponse(call: Call, response: Response) {
-//                println(response.code)
-//            }
-//        })val textview = findViewById<TextView>(R.id.textview)
-//            textview?.text = response.code.toString()
-
     }
 }
