fa6131de0755e6203aac6eca07d2d1c76d86c2fd
Jennifer Nord <5363215+JenniNord@users.noreply.github.com>
2020-05-04 12:15:12 +0200

    Change webserver implementation
    Changed from using just WebServer.h to this example: https://github.com/DIT112-V20/kotlin-app-arduino-sketch-ci/blob/master/arduino/SmartcarWebserver/SmartcarWebserver.ino
    Added in some logic for handling requests such as /random and /dance?id=1. Also changed some comments and moved the random dance move logic into a separate method for now as we're thinking of randomizing dances in the app logic and not on the car itself. As it is now, the random dance will loop 5 times which means that 5 moves will be performed by the car.
    
    The mDNS name of the car is set to be djsmartcar and should be accessible through djsmartcar.local.
    
    Resolves #30

--- src/carLogic/carLogic.ino
+++ src/carLogic/carLogic.ino
@@ -1,12 +1,11 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com  
-*********/
-
-// Load Wi-Fi library
-#include <WiFi.h>
+#include <ESPmDNS.h>
 #include <Smartcar.h>
+#include <WiFi.h>
+#include <WebServer.h>
 
+/**
+ * SMARTCAR VARIABLES
+ */
 BrushedMotor leftMotor(smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(smartcarlib::pins::v2::rightMotorPins);
 DifferentialControl control(leftMotor, rightMotor);
@@ -23,143 +22,96 @@ const unsigned long leftPulsesPerMeter = 943;
 const unsigned long rightPulsesPerMeter = 981;
 DirectionalOdometer leftOdometer (smartcarlib::pins::v2::leftOdometerPins, [](){
     leftOdometer.update();
-  },
-  leftPulsesPerMeter);
+  }, leftPulsesPerMeter);
 DirectionalOdometer rightOdometer (smartcarlib::pins::v2::rightOdometerPins, [](){
     rightOdometer.update();
-  },
-  rightPulsesPerMeter);
+  }, rightPulsesPerMeter);
+
+SmartCar car(control, gyro, leftOdometer, rightOdometer);
 
 /**
- * NETWORK DETAILS SETUP FOR WIFI CONNECTION
+ * NETWORK VARIABLES
  */
   
 // Replace with your network credentials
 const char* ssid     = "TheGaulle";
 const char* password = "canihaz#";
 
-// Set web server port number to 80
-WiFiServer server(80);
-
-// Variable to store the HTTP request
-String header;
-
-SmartCar car(control, gyro, leftOdometer, rightOdometer);
+WebServer server(80);
+String header; // Variable to store the HTTP request
 
 void setup() {
-
-Serial.begin(115200);
-
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  
+  Serial.begin(115200);
+  
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
+  Serial.println("");
+  
+  // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  // Print local IP address and start web server
   Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  server.begin();
+  
+  if (MDNS.begin("djsmartcar")) {
+    Serial.println("MDNS responder started");
+  }
+  
+  server.on("/dance", []() {
+    const auto arguments = server.args();
+  
+    for (auto i = 0; i < arguments; i++) {
+      const auto command = server.argName(i);
+      
+      if (command == "id") {
+        handleInput(server.arg(i).toInt());
+      }
+    }
+  
+    server.send(200, "text/json", "[{'id':'1'}]"); //Not sure if this works
+  });
 
+  server.on("/random", []() {
+    const auto arguments = server.args();
+    randomDance();
+    
+    server.send(200, "text/json", "[{'id':'1'}]"); //Not sure if this works
+  });
+  
+  server.onNotFound(
+    []() { server.send(404, "text/plain", "Unknown command"); });
+  
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
-
-/**
- * WIFI CONNECTION SETUP
- */
-
- WiFiClient client = server.available();   // Listen for incoming clients
-
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /dance?id=1") >= 0) {
-              Serial.println("Start Spinning");
-            } else if (header.indexOf("GET /26/off") >= 0) {
-              Serial.println("Stop Spinning");
-            } else if (header.indexOf("GET /27/on") >= 0) {
-              Serial.println("GPIO 27 on");
-            } else if (header.indexOf("GET /27/off") >= 0) {
-              Serial.println("GPIO 27 off");
-            }
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #555555;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP32 Web Server</h1>");
-               
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
-
-/**
- * DANCE MOVES 
- */
+  server.handleClient();
   
-    unsigned int distance = sensor.getDistance();
-    
-    if (distance != 0 && distance < 20){
-      car.setSpeed(0);
-    } else {
-      int danceID = random(1, 5);
-      handleInput(danceID);
-    }
-
-    delay(1000);
+  /* unsigned int distance = sensor.getDistance();
+    if (distance != 0 && distance < 20){ 
+    car.setSpeed(0); 
+    } */
 }
 
 /**
  * DANCE MOVE LOGIC
  */
+
+/**
+ * Random dance, loops 5 times
+ */
+void randomDance() {
+  for (int i = 0; i < 5; i++) {
+    handleInput(random(1, 5));
+  }
+}
 
 /** 
  *  Takes an integer and switch case determines which dance to perform
@@ -181,6 +133,8 @@ void handleInput(int danceID) {
     default:
       break;
   }
+
+  delay(1000);
 }
 
 /**
@@ -321,6 +275,9 @@ void shake(int speed) {
   car.setSpeed(0);
 }
 
+/**
+ * Macarena dance move
+ */
 void macarena(int speed) {
   long startingPoint = 0;
   int steps = 1;
