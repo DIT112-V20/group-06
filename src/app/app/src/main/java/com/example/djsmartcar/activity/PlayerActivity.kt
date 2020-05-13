e6527bade3e811fac7ddf30968568369f9946bcc
hashmati <negin_h@icloud.com>
2020-05-13 14:58:15 +0200

    Update play and pause buttons
    Created play, pause and stop buttons and implemented the play and pause buttons into the app. We also resized the track image and removed some commented out code.

--- src/app/app/src/main/java/com/example/djsmartcar/activity/PlayerActivity.kt
+++ src/app/app/src/main/java/com/example/djsmartcar/activity/PlayerActivity.kt
@@ -37,12 +37,12 @@ class PlayerActivity : AppCompatActivity() {
     }
 
     private fun setupListeners() {
-        playButton.setOnClickListener {
+        pauseSnippet.setOnClickListener {
             SpotifyService.play("spotify:playlist:37i9dQZF1DWYMvTygsLWlG")
             showPauseButton()
         }
 
-        pauseButton.setOnClickListener {
+        pauseSnippet.setOnClickListener {
             SpotifyService.pause()
             showResumeButton()
         }
@@ -60,54 +60,20 @@ class PlayerActivity : AppCompatActivity() {
     }
 
     private fun showPlayButton() {
-        playButton.visibility = View.VISIBLE
-        pauseButton.visibility = View.GONE
+        playSnippet.visibility = View.VISIBLE
+        pauseSnippet.visibility = View.GONE
         resumeButton.visibility = View.GONE
     }
 
     private fun showPauseButton() {
-        playButton.visibility = View.GONE
-        pauseButton.visibility = View.VISIBLE
+        playSnippet.visibility = View.GONE
+        pauseSnippet.visibility = View.VISIBLE
         resumeButton.visibility = View.GONE
     }
 
     private fun showResumeButton() {
-        playButton.visibility = View.GONE
-        pauseButton.visibility = View.GONE
+        playSnippet.visibility = View.GONE
+        pauseSnippet.visibility = View.GONE
         resumeButton.visibility = View.VISIBLE
     }
-
-    /*override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player)
-        setupViews()
-        setupListeners()
-    }*/
-
-    /*private fun setupViews () {
-        SpotifyService.playingState {
-            when(it) {
-                PlayingState.PLAYING -> showPauseButton()
-                PlayingState.STOPPED -> showPlayButton()
-                PlayingState.PAUSED -> showResumeButton()
-            }
-        }
-    }*/
-
-    /*private fun setupListeners() {
-        playButton.setOnClickListener {
-            SpotifyService.play("spotify:album:5L8VJO457GXReKVVfRhzyM")
-            showPauseButton()
-        }
-
-        pauseButton.setOnClickListener {
-            SpotifyService.pause()
-            showResumeButton()
-        }
-
-        resumeButton.setOnClickListener {
-            SpotifyService.resume()
-            showPauseButton()
-        }
-    }*/
 }
